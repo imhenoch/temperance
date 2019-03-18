6f188a988f8c5a95a3b86374ff4aa290cff7ae68
Isidro Henoch <isidro.henoch@gmail.com>
2019-03-18 17:55:48 -0600

    Changes temperature view model

--- lib/view_models.dart
+++ lib/view_models.dart
@@ -5,23 +5,36 @@ enum ScaleType { CELSIUS, FAHRENHEIT, KELVIN }
 class TemperatureViewModel {
   final double degrees;
   final void Function(double degrees) onValueChanged;
+  final void Function(String input) onInputChanged;
 
-  TemperatureViewModel({this.degrees, this.onValueChanged});
+  TemperatureViewModel(
+      {this.degrees, this.onValueChanged, this.onInputChanged});
 
   static TemperatureViewModel fromStore(
-      AppState state, ScaleType scale, Function onValueChanged) {
+      {AppState state,
+      ScaleType scale,
+      // ignore: avoid_init_to_null
+      Function onValueChanged = null,
+      // ignore: avoid_init_to_null
+      Function onInputChanged = null}) {
     switch (scale) {
       case ScaleType.CELSIUS:
         return TemperatureViewModel(
-            degrees: state.celsius, onValueChanged: onValueChanged);
+            degrees: state.celsius,
+            onValueChanged: onValueChanged,
+            onInputChanged: onInputChanged);
         break;
       case ScaleType.FAHRENHEIT:
         return TemperatureViewModel(
-            degrees: state.fahrenheit, onValueChanged: onValueChanged);
+            degrees: state.fahrenheit,
+            onValueChanged: onValueChanged,
+            onInputChanged: onInputChanged);
         break;
       case ScaleType.KELVIN:
         return TemperatureViewModel(
-            degrees: state.kelvin, onValueChanged: onValueChanged);
+            degrees: state.kelvin,
+            onValueChanged: onValueChanged,
+            onInputChanged: onInputChanged);
         break;
     }
     return TemperatureViewModel();

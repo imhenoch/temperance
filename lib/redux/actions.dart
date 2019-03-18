0b896ac02171d3b0964c6e6e48a1a3ef550aa353
Isidro Henoch <isidro.henoch@gmail.com>
2019-03-18 16:00:59 -0600

    Adds redux actions

---
+++ lib/redux/actions.dart
@@ -0,0 +1,17 @@
+class ChangeCelsiusAction {
+  final double degrees;
+
+  ChangeCelsiusAction({this.degrees});
+}
+
+class ChangeFahrenheitAction {
+  final double degrees;
+
+  ChangeFahrenheitAction({this.degrees});
+}
+
+class ChangeKelvinAction {
+  final double degrees;
+
+  ChangeKelvinAction({this.degrees});
+}
